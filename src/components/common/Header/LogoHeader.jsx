a9ca2ea21b8a6515a50b27318335c6601c9d1ef7
jsunbin <jsnb6n@gmail.com>
2023-07-25 19:37:16 +0900

    💄 Design: 로고가 있는 헤더 컴포넌트 마크업 #13

---
+++ src/components/common/Header/LogoHeader.jsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+import LogoImg from '../../../assets/images/Logo-hodu.svg';
+
+export default function LogoHeader() {
+  return (
+    <header css={headerStyles}>
+      <h1 className="logo-title">
+        <a href="/#">
+          <img src={LogoImg} alt="호두" />
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+const headerStyles = css({
+  width: '100%',
+  padding: '100px 0 90px',
+  '& a': {
+    display: 'block',
+    width: '238px',
+    margin: '0 auto',
+  },
+  '& .logo-title img': {
+    display: 'block',
+    width: '100%',
+    height: '74px',
+  },
+});
