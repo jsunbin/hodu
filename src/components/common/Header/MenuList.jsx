89a49b95457817bdc56f2ae03f7a833ab272be89
jsunbin <jsnb6n@gmail.com>
2023-08-18 23:57:26 +0900

    💄 Style: 로그인 상태에 따른 Header 컴포넌트의 MenuList 컴포넌트 마크업 수정 #61

--- src/components/common/Header/MenuList.jsx
+++ src/components/common/Header/MenuList.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { css } from '@emotion/react';
 import Button from '../../Button/Button';
 import shoppingCartIcon from '../../../assets/images/icon-shopping-cart.svg';
@@ -30,7 +31,7 @@ const ulStyles = {
     '&.menu-cart::before': {
       background: `url(${shoppingCartIcon}) top no-repeat`,
     },
-    '&.menu-mypage::before': {
+    '&.menu-mypage::before, &.menu-login::before': {
       background: `url(${userIcon}) top no-repeat`,
     },
   },
@@ -39,37 +40,82 @@ const ulStyles = {
   },
 };
 
-export default function MenuList({ isSeller = false }) {
-  return (
+export default function MenuList({ isLogin = false, isSeller = false }) {
+  return !isLogin ? (
+    // 로그인 X
     <nav>
       <ul css={ulStyles} className="menu-list">
-        {isSeller ? (
-          <>
-            <li>
-              <a href="/#" className="menu-cart">
-                장바구니
-              </a>
-            </li>
-            <li>
-              <Button size="ms" icon={true}>
-                {/* 나머지 판매자 센터 Button 코드 */}
-              </Button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <a href="/#" className="menu-cart">
-                장바구니
-              </a>
-            </li>
-            <li>
-              <a href="/#" className="menu-mypage">
-                마이페이지
-              </a>
-            </li>
-          </>
-        )}
+        <li>
+          <Link to="/cart" className="menu-cart">
+            장바구니
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="menu-login">
+            로그인
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  ) : !isSeller ? (
+    // 구매회원으로 로그인
+    <nav>
+      <ul css={ulStyles} className="menu-list">
+        <li>
+          <Link to="/cart" className="menu-cart">
+            장바구니
+          </Link>
+        </li>
+        <li>
+          <Link to="/mypage" className="menu-mypage">
+            마이페이지
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  ) : (
+    // 판매회원으로 로그인
+    <nav>
+      <ul css={ulStyles} className="menu-list">
+        <li>
+          <Link to="/cart" className="menu-cart">
+            마이페이지
+          </Link>
+        </li>
+        <li>
+          <Button size="ms" icon={true} className="menu-seller-center">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8 2.66669L4 8.00002V26.6667C4 27.3739 4.28095 28.0522 4.78105 28.5523C5.28115 29.0524 5.95942 29.3334 6.66667 29.3334H25.3333C26.0406 29.3334 26.7189 29.0524 27.219 28.5523C27.719 28.0522 28 27.3739 28 26.6667V8.00002L24 2.66669H8Z"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M4 8H28"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M21.3337 13.3333C21.3337 14.7478 20.7718 16.1044 19.7716 17.1045C18.7714 18.1047 17.4148 18.6666 16.0003 18.6666C14.5858 18.6666 13.2293 18.1047 12.2291 17.1045C11.2289 16.1044 10.667 14.7478 10.667 13.3333"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            판매자센터
+          </Button>
+        </li>
       </ul>
     </nav>
   );
