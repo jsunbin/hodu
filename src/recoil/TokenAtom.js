1a4211bb8941ad7c2392e010ea350d0968deb41e
jsunbin <jsnb6n@gmail.com>
2023-08-22 21:24:03 +0900

    🐛  Fix: 로컬스토리지에 토큰 값 저장하는 코드 추가하여 로그인 유지 이슈 해결 #67

--- src/recoil/TokenAtom.js
+++ src/recoil/TokenAtom.js
@@ -1,9 +1,16 @@
 import { atom, selector } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist({
+  key: 'token',
+  storage: localStorage,
+});
 
 // 토큰
 export const TokenAtom = atom({
   key: 'TokenAtom',
   default: undefined,
+  effects_UNSTABLE: [persistAtom],
 });
 
 // 로그인 여부를 확인하는 Selector
