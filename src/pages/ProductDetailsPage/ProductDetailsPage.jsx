3c123d39cbf8231db5c8bad6fdb8b7eb368bc63d
jsunbin <jsnb6n@gmail.com>
2023-08-24 00:18:42 +0900

    ✨ Feat: 상품 상세 페이지 내 장바구니 기능 구현 #70

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,33 +1,68 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState, useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { css } from '@emotion/react';
 import { productsDetailAPI } from '../../api/productsAPI';
+import { addCartAPI, cartListAPI } from '../../api/cartAPI';
 import { isUserSeller } from '../../recoil/LoginAtom';
 import { isLoginSelector, TokenAtom } from '../../recoil/TokenAtom';
 import { AmountAtom } from '../../recoil/AmountAtom';
+import {
+  closeModalSelector,
+  modalStateAtom,
+  openModalSelector,
+} from '../../recoil/ModalAtom';
 import Header from '../../components/common/Header/Header';
 import Footer from '../../components/common/Footer/Footer';
 import Price from '../../components/Price/Price';
 import Amount from '../../components/common/Amount/Amount';
 import Button from '../../components/Button/Button';
 import TabMenu from '../../components/TabMenu/TabMenu';
+import Modal from '../../components/Modal/Modal';
+import ConfirmModal from '../../components/Modal/ConfirmModal/ConfirmModal';
 
 export default function ProductDetailsPage() {
+  const navigate = useNavigate();
   const accessToken = useRecoilValue(TokenAtom);
   const isLogin = useRecoilValue(isLoginSelector);
   const isSeller = useRecoilValue(isUserSeller);
   const amount = useRecoilValue(AmountAtom);
+  const modalState = useRecoilValue(modalStateAtom);
+  const setOpenModal = useSetRecoilState(openModalSelector);
+  const setCloseModal = useSetRecoilState(closeModalSelector);
   const [item, setItem] = useState([]);
   const [price, setPrice] = useState('0');
   const [deliveryMethod, setDeliveryMethod] = useState('택배배송');
   const [deliveryFee, setDeliveryFee] = useState('무료배송');
+  const [isItemInCart, setIsItemInCart] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [loadingError, setLoadingError] = useState(null);
+  const productId = window.location.pathname.replace('/product/', '');
 
-  const handleCartClick = () => {
-    console.log('장바구니 담기');
-    console.log('이동 확인 모달 띄우기');
+  const handleCartClick = async () => {
+    const data = await getCartList(accessToken);
+
+    const { results } = data.data;
+
+    console.log('results, ', results);
+    console.log(productId);
+
+    const isItemInResults = results.find(
+      item => item.product_id === Number(productId),
+    );
+
+    if (isItemInResults) {
+      setIsItemInCart(true);
+      setOpenModal();
+    } else {
+      await addItemToCart().then(setIsItemInCart(false)).then(setOpenModal());
+    }
+  };
+
+  const handleGoToCartClick = () => {
+    setCloseModal();
+    navigate('/cart');
   };
 
   const getProductsDetails = async productId => {
@@ -48,9 +83,40 @@ export default function ProductDetailsPage() {
     }
   };
 
-  useEffect(() => {
-    const productId = window.location.pathname.replace('/product/', '');
+  // 장바구니 목록 가져오기
+  const getCartList = async accessToken => {
+    try {
+      const data = await cartListAPI(accessToken);
 
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 장바구니에 담기
+  const addItemToCart = async () => {
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+
+      const data = await addCartAPI({
+        accessToken,
+        productId,
+        amount,
+        isAvailable: true,
+      });
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
     getProductsDetails(productId);
   }, []);
 
@@ -66,6 +132,7 @@ export default function ProductDetailsPage() {
   return (
     <>
       <Header isLogin={isLogin} isSeller={isSeller} />
+
       {!isLoading ? (
         <main css={mainStyles}>
           <section css={firstWrapDivStyles}>
@@ -82,37 +149,61 @@ export default function ProductDetailsPage() {
               <span css={deliveryOptionsSpanStyles}>
                 {deliveryMethod} / {deliveryFee}
               </span>
-              <Amount max={3} />
-              <div css={productTotalStyles}>
-                <span className="title">총 상품 금액</span>
-                <div css={orderDetailsStyles}>
-                  <span css={orderAmountStyles} className="order-amount">
-                    총 수량 <strong>{amount}</strong>개
-                  </span>
-                  <Price size="lg" color="#21BF48">
-                    {price}
-                  </Price>
-                </div>
-              </div>
-              <div css={btnGroupsStyles}>
-                <Button size="md" width="416px" href={'/payment'}>
-                  바로구매
+
+              {item.stock === 0 ? (
+                <Button disabled width="630px">
+                  품절
                 </Button>
-                <Button
-                  size="md"
-                  color="dark"
-                  width="200px"
-                  onClickEvent={handleCartClick}
-                >
-                  장바구니
-                </Button>
-              </div>
+              ) : (
+                <>
+                  <Amount max={item.stock} />
+                  <div css={productTotalStyles}>
+                    <span className="title">총 상품 금액</span>
+                    <div css={orderDetailsStyles}>
+                      <span css={orderAmountStyles} className="order-amount">
+                        총 수량 <strong>{amount}</strong>개
+                      </span>
+                      <Price size="lg" color="#21BF48">
+                        {price}
+                      </Price>
+                    </div>
+                  </div>
+                  <div css={btnGroupsStyles}>
+                    <Button size="md" width="416px" href={'/payment'}>
+                      바로구매
+                    </Button>
+                    <Button
+                      size="md"
+                      color="dark"
+                      width="200px"
+                      onClickEvent={handleCartClick}
+                    >
+                      장바구니
+                    </Button>
+                  </div>
+                </>
+              )}
             </section>
           </section>
           <section css={productDetailsWrapStyles}>
             <TabMenu />
             <div className="menu-content"></div>
           </section>
+          {modalState.isOpen &&
+            (isItemInCart ? (
+              <Modal yesOnClickEvent={handleGoToCartClick}>
+                이미 장바구니에 있는 상품입니다.
+                <br />
+                장바구니로 이동하시겠습니까?
+              </Modal>
+            ) : (
+              <ConfirmModal
+                btnContent={'장바구니로 가기'}
+                btnClickEvent={handleGoToCartClick}
+              >
+                장바구니에 상품이 담겼습니다
+              </ConfirmModal>
+            ))}
         </main>
       ) : (
         <div>Loading...</div>
@@ -125,6 +216,7 @@ export default function ProductDetailsPage() {
 const mainStyles = css`
   width: 100%;
   margin: 80px auto;
+  position: relative;
 `;
 
 const firstWrapDivStyles = css({
